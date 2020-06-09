eab629c10b90809cc2e6b08b6d8e89fe0c085633
Michael Threels <michaelthreels@gmail.com>
2020-06-10 01:07:55 +0200

    included the original bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# def bubble_sort(array)
+#   iterations = array.length - 1
+#   iterations.times do
+#     iterations.times do |counter|
+#       array[counter], array[counter + 1] = array[counter + 1], array[counter] if array[counter] > array[counter + 1]
+#       p array
+#     end
+#   end
+# end
+
+# bubble_sort([6, 5, 4, 3, 2, 1])
+
 def bubble_sort_by(array)
   iterations = array.length - 1
   iterations.times do |counter|
