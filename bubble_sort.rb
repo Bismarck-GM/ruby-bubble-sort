b9bf67cf79786d3def80b31b3f6d642f68ed809b
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-06-10 13:01:48 -0300

    Uncommented first bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
-# def bubble_sort(array)
-#   iterations = array.length - 1
-#   iterations.times do
-#     iterations.times do |counter|
-#       array[counter], array[counter + 1] = array[counter + 1], array[counter] if array[counter] > array[counter + 1]
-#       p array
-#     end
-#   end
-# end
+def bubble_sort(array)
+  iterations = array.length - 1
+  iterations.times do
+    iterations.times do |counter|
+      array[counter], array[counter + 1] = array[counter + 1], array[counter] if array[counter] > array[counter + 1]
+    end
+  end
+end
 
-# bubble_sort([6, 5, 4, 3, 2, 1])
+bubble_sort([6, 5, 4, 3, 2, 1])
 
 def bubble_sort_by(array)
   iterations = array.length - 1
